603d27d426ad02deaa4ce4dc7f3c273691c6a91d
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-11-14 10:24:32 -0800

    Update cron job url

--- cron.js
+++ cron.js
@@ -2,7 +2,7 @@
 const cron = require("cron");
 const https = require("https");
 
-const backendUrl = "https://pocket-money.onrender.com:10000/health";
+const backendUrl = "https://pocket-money.onrender.com/health";
 const job = new cron.CronJob("*/14 * * * *", function () {
   //This function will be executed every 14 minutes
   console.log("Checking server status...");
