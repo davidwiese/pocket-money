2cd8e3b1b051fd00d0832bbf3089dfb0dbad2c2c
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-11-19 14:44:25 -0800

    Update port console log

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  })
+  }),
 );
 
 // Passport middleware
@@ -54,5 +54,5 @@ const { job } = require("./cron");
 job.start();
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
+  console.log(`Server is running on port: ${process.env.PORT}`);
 });
